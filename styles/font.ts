3f603d558f6ea50296e8759568da2a7dd91afa6d
Janny0222 <villarueljanmichael@gmail.com>
2024-05-09 16:41:13 +0800

    add features and style

--- styles/font.ts
+++ styles/font.ts
@@ -12,4 +12,4 @@ export const roboto_flex = Roboto_Flex({subsets: ['latin'], weight: ["400", "500
 export const bsdisplay = Big_Shoulders_Display({subsets: ['latin'], weight: ["400", "500", "600"]})
 export const roboto = Roboto({subsets: ['latin'], weight: ["100", "300", "500", "700"]})
 
-export const lato = Lato({subsets: ['latin'], weight: ["400"]})
+export const lato = Lato({subsets: ['latin'], weight: ["400", "700", "900"]})
